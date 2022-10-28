a4891c1ad8edc7ca27628ee713990921fe64529f
janisoteps <janis.oteps@gmail.com>
2022-10-29 00:20:18 +0200

    product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {ProductListComponent} from "./product-list/product-list.component";
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: ProductListComponent
+    }
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
