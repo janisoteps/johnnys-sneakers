e2c4b08ed9f85baaf74bc13cf96c7e68e9c74be7
janisoteps <janis.oteps@gmail.com>
2022-10-29 23:49:05 +0200

    sneakers cards

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,7 +7,6 @@ import { products } from '../products';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-
 export class ProductListComponent {
     products = products;
 
--- src/app/products.ts
+++ src/app/products.ts
@@ -3,25 +3,93 @@ export interface Product {
     name: string;
     price: number;
     description: string;
+    img: string;
 }
 
 export const products: Array<Product> = [
     {
         id: 1,
-        name: 'Phone XL',
-        price: 799,
-        description: 'A large phone with one of the best screens'
+        name: 'Capece D',
+        price: 399,
+        description: 'Capece D is a sneaker that will make you feel like you\'re walking on air. It has a comfortable and stylish design that is perfect for everyday wear. The Capece D has a breathable mesh upper that will keep your feet cool and dry all day long. The cushioning in the sneaker is unbeatable, and the rubber outsole provides traction and durability. You\'ll love the way the Capece D looks and feels, and you\'ll never want to take them off!',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/danilo-capece-NoVnXXmDNi0-unsplash.jpg'
     },
     {
         id: 2,
-        name: 'Phone Mini',
-        price: 699,
-        description: 'A great phone with one of the best cameras'
+        name: 'Irene Credence',
+        price: 199,
+        description: 'Irene Credence is a beautiful sneaker that looks great on everyone. It\'s perfect for a night out or a day at the office. It\'s versatile and stylish, and it\'s also comfortable and affordable.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/irene-kredenets-dwKiHoqqxk8-unsplash.jpg'
     },
     {
         id: 3,
-        name: 'Phone Standard',
+        name: 'Jo B',
+        price: 149,
+        description: 'Jo B. is a comfortable, stylish sneaker that\'s perfect for everyday wear. With a soft, breathable upper and a cushioned sole, it\'s a shoe you can rely on for all-day comfort. Plus, the trendy design means you\'ll always look good in Jo B.!',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/joseph-barrientos-4qSb_FWhHKs-unsplash.jpg'
+    },
+    {
+        id: 4,
+        name: 'Gepard K',
+        price: 199,
+        description: 'Gepard K is a new sneaker from the popular sportswear brand, Nike. This shoe is designed for speed and comfort, with a sleek silhouette that is perfect for running or working out. The Gepard K has a lightweight, breathable upper that will keep your feet cool and comfortable, and the Nike Zoom Air technology in the sole provides responsive cushioning. The rubber outsole provides traction and durability, so you can wear these shoes on any surface.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/kris-gerhard-0BKZfcamvGg-unsplash.jpg'
+    },
+    {
+        id: 5,
+        name: 'Kallergis',
         price: 299,
-        description: ''
+        description: 'Kallergis is a new sneaker from Greece that is taking the world by storm. With its sleek design and comfortable fit, it\'s no wonder why everyone is talking about this shoe. Kallergis is perfect for any activity, whether you\'re going for a run or just running errands.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/lefteris-kallergis-j1GiPlvSGWI-unsplash.jpg'
+    },
+    {
+        id: 6,
+        name: 'Pissioli MF',
+        price: 499,
+        description: 'Pissioli MF is a high-quality sneaker that is perfect for those who want to feel comfortable and stylish at the same time. With a soft and comfortable upper, this sneaker will keep your feet feeling great all day long. Additionally, the Pissioli MF has a durable outsole that can take on any surface, making it perfect for any activity.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/maria-fernanda-pissioli-DTZV8WDM1Ho-unsplash.jpg'
+    },
+    {
+        id: 7,
+        name: 'Omarwitch',
+        price: 199,
+        description: 'Omarwitch is a sneaker that was made for people who love the outdoors. It is comfortable, stylish, and has a lot of features that make it perfect for people who love to hike, camp, and explore.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/omar-prestwich-jLEGurepDco-unsplash.jpg'
+    },
+    {
+        id: 8,
+        name: 'Pat Kwon',
+        price: 339,
+        description: 'The Pat Kwon sneaker from our e-shop is perfect for the modern man on the go. This stylish sneaker features a sleek design with a comfortable fit, making it ideal for everyday wear. The Pat Kwon sneaker is also versatile enough to be dressed up or down, making it a must-have in your wardrobe.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/pat-kwon-EJTjetc8tPs-unsplash.jpg'
+    },
+    {
+        id: 9,
+        name: 'Plomper',
+        price: 299,
+        description: 'The Plomper sneaker is a comfortable and stylish shoe that is perfect for everyday wear. With a padded collar and tongue, this shoe provides support and cushioning for your feet, while the breathable mesh upper keeps them cool and dry. The Plomper also features a durable rubber outsole that provides traction and stability, making it a great choice for walking or running.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/ryan-plomp-jvoZ-Aux9aw-unsplash.jpg'
+    },
+    {
+        id: 10,
+        name: 'Plomper GT',
+        price: 469,
+        description: 'The Plomper GT is a bigger and more luxurious sneaker compared to the normal sneaker model Plomper. It is made with high quality materials and is very comfortable to wear. This sneaker is perfect for those who want to make a statement and stand out from the crowd.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/ryan-plomp-PGTO_A0eLt4-unsplash.jpg'
+    },
+    {
+        id: 11,
+        name: 'Usama A',
+        price: 729,
+        description: '\n' +
+            'Introducing the Usama A sneaker, the first Japanese made sneaker made from whale skin. This luxurious sneaker is handcrafted from the finest whale skin, making it ultra soft and comfortable. The unique texture of the whale skin gives the Usama A sneaker a one-of-a-kind look that is sure to turn heads. The Usama A sneaker is also environmentally friendly, as whale skin is a sustainable and renewable resource.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/usama-akram-kP6knT7tjn4-unsplash.jpg'
+    },
+    {
+        id: 12,
+        name: 'Xavier',
+        price: 29,
+        description: 'Introducing Xavier, the first ever Spanish-made sneaker made from Jamon! This unique sneaker is handcrafted from premium quality Jamon, ensuring that each pair is unique and of the highest quality. With a comfortable fit and a stylish design, Xavier is perfect for any fashion-savvy individual who wants to make a statement.',
+        img: 'https://johnnys-sneakers.s3.eu-central-1.amazonaws.com/xavier-teo-SxAXphIPWeg-unsplash.jpg'
     }
 ];
